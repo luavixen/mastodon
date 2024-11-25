ce7434ddff4ad450e796dd1fde061a79f2da9425
Lua MacDougall <lua@foxgirl.dev>
2024-11-25 00:23:38 +0000

    Increase rate limits again

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -66,15 +66,15 @@ class Rack::Attack
     IpBlock.blocked?(req.remote_ip)
   end
 
-  throttle('throttle_authenticated_api', limit: 3_000, period: 5.minutes) do |req|
+  throttle('throttle_authenticated_api', limit: 30_000, period: 5.minutes) do |req|
     req.authenticated_user_id if req.api_request?
   end
 
-  throttle('throttle_per_token_api', limit: 300, period: 5.minutes) do |req|
+  throttle('throttle_per_token_api', limit: 1_000, period: 5.minutes) do |req|
     req.authenticated_token_id if req.api_request?
   end
 
-  throttle('throttle_unauthenticated_api', limit: 300, period: 5.minutes) do |req|
+  throttle('throttle_unauthenticated_api', limit: 1_000, period: 5.minutes) do |req|
     req.throttleable_remote_ip if req.api_request? && req.unauthenticated?
   end
 
@@ -82,7 +82,7 @@ class Rack::Attack
     req.authenticated_user_id if req.post? && req.path.match?(%r{\A/api/v\d+/media\z}i)
   end
 
-  throttle('throttle_media_proxy', limit: 30_000, period: 10.minutes) do |req|
+  throttle('throttle_media_proxy', limit: 50_000, period: 10.minutes) do |req|
     req.throttleable_remote_ip if req.path.start_with?('/media_proxy')
   end
 
@@ -90,7 +90,7 @@ class Rack::Attack
     req.throttleable_remote_ip if req.post? && req.path == '/api/v1/accounts'
   end
 
-  throttle('throttle_authenticated_paging', limit: 600, period: 15.minutes) do |req|
+  throttle('throttle_authenticated_paging', limit: 1_000, period: 15.minutes) do |req|
     req.authenticated_user_id if req.paging_request?
   end
 
